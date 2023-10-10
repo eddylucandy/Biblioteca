dabc83c53040c557122d756497938881a60a5b90
ddy_Lucandy^ <babago-ys@outlook.es>
2023-10-10 15:06:59 +0200

    Menu biblioteca
    He agregado un metodo para el menu biblioteca

--- main.cpp
+++ main.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <list>
+
+using namespace std;
+
+
+void mostrarMenu() {
+    cout << endl
+         << endl
+         << "---------------- M E N U ----------------" << endl;
+    cout << "¦                                       ¦" << endl;
+    cout << "¦  1. Libros disponibles                ¦" << endl;
+    cout << "¦                                       ¦" << endl;
+    cout << "¦  2. Historial de un usuario           ¦" << endl;
+    cout << "¦                                       ¦" << endl;
+    cout << "¦  3. Prestar libro                     ¦" << endl;
+    cout << "¦                                       ¦" << endl;
+    cout << "¦  4. Devolver libro                    ¦" << endl;
+    cout << "¦                                       ¦" << endl;
+    cout << "¦  5. Consulta de libros prestado       ¦" << endl;
+    cout << "¦                                       ¦" << endl;
+    cout << "¦  6. Salir                             ¦" << endl;
+    cout << "¦                                       ¦" << endl;
+    cout << "-----------------------------------------*" << endl
+         << endl;
+}
 
 int main() {
     std::cout << "Hola mi gente" << std::endl;
